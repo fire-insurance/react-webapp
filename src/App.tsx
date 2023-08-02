cc37be3943865a4375a1903b3b61838b3138d704
Dmitry Duda <dmitrij.snake2000@yandex.ru>
2023-08-02 11:01:15 +1000

    add: clsx, styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
-import './index.scss';
+import './styles/global.scss';
 import { Link } from 'react-router-dom';
 import { AboutPageAsync } from './pages/aboutPage/aboutPage.async';
 import { MainPageAsync } from './pages/mainPage/mainPage.async';
 import { Suspense } from 'react';
+import clsx from 'clsx';
 
 export const App = () => (
-    <main className={'app'}>
+    <main className={clsx('app', 'light-theme')}>
         <Link to={'/'}>Главная</Link>
         <Link to={'/about'}>О сайте</Link>
         <Suspense fallback={<div>Loading...</div>}>
