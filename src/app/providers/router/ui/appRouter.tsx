6378aae9c5d942eeafa20afb4b64778333451278
Dmitry Duda <dmitrij.snake2000@yandex.ru>
2023-09-05 18:07:06 +1000

    ad: route loader

--- src/app/providers/router/ui/appRouter.tsx
+++ src/app/providers/router/ui/appRouter.tsx
@@ -1,23 +1,31 @@
 import { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { routeConfig } from '../model/const/routeConfig';
+import { Loader } from '@/z-shared/ui/loader/loader';
 
 export const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            {
+
+    <Routes>
+        {
                 Object.values(routeConfig).map(({ element, path }) => (
                     <Route
                         key={path}
                         path={path}
                         element={(
+
                             <div className={'page-wrapper'}>
-                                {element}
+                                <Suspense fallback={(
+                                    <div className={'suspense-wrapper'}>
+                                        <Loader/>
+                                    </div>
+                                )}
+                                >
+                                    {element}
+                                </Suspense>
                             </div>
                         )}
                     />
                 ))
             }
-        </Routes>
-    </Suspense>
+    </Routes>
 );
