6ea4bcb444f0074f0cbc604115362ac254d2b030
Dmitry Duda <dmitrij.snake2000@yandex.ru>
2023-09-05 21:33:07 +1000

    add: jest

---
+++ config/jest/jest.config.ts
@@ -0,0 +1,35 @@
+export default {
+    // Automatically clear mock calls, instances and results before every test
+    clearMocks: true,
+
+    // An array of regexp pattern strings used to skip coverage collection
+    coveragePathIgnorePatterns: [
+        '\\\\node_modules\\\\',
+    ],
+
+    // The test environment that will be used for testing
+    testEnvironment: 'jsdom',
+
+    // An array of directory names to be searched recursively up from the requiring module's location
+    moduleDirectories: [
+        'node_modules',
+    ],
+
+    // An array of file extensions your modules use
+    moduleFileExtensions: [
+        'js',
+        'jsx',
+        'ts',
+        'tsx',
+        'json',
+        'node',
+    ],
+
+    // The glob patterns Jest uses to detect test files
+    testMatch: [
+        '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
+    ],
+
+    // The root directory that Jest should scan for tests and modules within
+    rootDir: '../../',
+};
